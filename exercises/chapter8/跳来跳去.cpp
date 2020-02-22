7c241aa0edb0a4af5e0feb84da1cd7d5c479ff3d
LeoChen008 <Leo_Chen008@163.com>
2020-02-22 20:19:21 +0800

    uva1621 cannot accepted by oj, but I think it's own fault.

---
+++ exercises/chapter8/跳来跳去.cpp
@@ -0,0 +1,67 @@
+/*
+* Created by LeoChen008
+*/
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+using namespace std;
+
+int a,b,c;
+int pos;
+
+inline
+void move_next(int len) {
+	pos += len;
+	printf(" %d", pos);
+}
+int main(void)
+{
+	int T;
+	cin >> T;
+	while (T--) {
+		cin >> a >> b >> c;
+		
+		pos = 0; 
+		
+		printf("0");
+		
+		if (c%3 == 0) {
+			move_next(2);
+			for (int i=0; i<c/3; ++i)move_next(3);
+			move_next(-1);
+			for (int i=0; i<c/3; ++i)move_next(-3);
+			move_next(2);
+			for (int i=0; i<c/3; ++i)move_next(3);
+			
+			a-=1; b-=2;
+		} 
+		else if (c%3 == 1) {
+			for (int i=0; i<c/3+1; ++i) move_next(3);
+			move_next(-2);
+			for (int i=0; i<c/3; ++i) move_next(-3);
+			move_next(1);
+			for (int i=0; i<c/3; ++i) move_next(3);
+			move_next(2);
+			
+			a-=1; b-=2; 
+		}
+		else if (c%3 == 2) {
+			for (int i=0; i<c/3+1; ++i) move_next(3);
+			move_next(-1);
+			for (int i=0; i<c/3; ++i)   move_next(-3);
+			move_next(-1);
+			for (int i=0; i<c/3+1; ++i) move_next(3);
+			
+			a-=2;
+		}
+		
+		for (int i=0; i<a-1; ++i) move_next(1);
+		
+		for (int i=0; i<ceil(b/2.0); ++i) move_next(2);
+		move_next(b%2==0 ? 1:-1);
+		for (int i=0; i<b/2; ++i) move_next(-2);
+		
+		printf("\n");
+	} 
+	return 0;
+}
